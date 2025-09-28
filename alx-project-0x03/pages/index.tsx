d7b9c6ad107bf32587253e745d3fea1101e8890a
akrate <oussamaakrate590@gmail.com>
2025-09-29 00:53:34 +0100

    test

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "@/components/layouts/Layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Welcome to Splash App</h1>
+      </div>
+    </Layout>
+  );
+}
